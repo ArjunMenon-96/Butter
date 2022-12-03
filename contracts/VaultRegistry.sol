ab110dd8b6bf0a3177f362d79ce4dee08f48a83b
Chanchal Delson <chanchal.d@netobjex.com>
2022-12-03 20:54:31 +0530

    ContractVer0.1.2-vaultRegistry change

--- contracts/VaultRegistry.sol
+++ contracts/VaultRegistry.sol
@@ -5,7 +5,7 @@ contract vaultRegistry {
     mapping (address => address) vaultMapping;
 
     function addVaultToUser(address vaultAddress) public {
-        require(vaultMapping[msg.sender]== "0x0000000000000000000000000000000000000000");
+        require(vaultMapping[msg.sender]== address(0));
         vaultMapping[msg.sender]= vaultAddress;
     }
 
